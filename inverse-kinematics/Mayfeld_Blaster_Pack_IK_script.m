0ed448f4e79df04c87bd4c50c86b45a263c8c282
Aditya Prakash <adityaprakash.23.09@gmail.com>
2021-03-30 20:18:33 +0530

    Update on script
    No actual working code yet. Only theoretical explanations about how to approach the problem.

--- inverse-kinematics/Mayfeld_Blaster_Pack_IK_script.m
+++ inverse-kinematics/Mayfeld_Blaster_Pack_IK_script.m
@@ -17,3 +17,18 @@ A36 = [ -sind(theta4+theta5)*cosd(theta6),  sind(theta4+theta5)*sind(theta6), co
 #will be a Geometric Circle case. Arbitrary physical human body 
 #measuments are considered here.Also other assumptions will be 
 #considered.
+syms shoulderToElbow; #length value based on a coordinate pair; 
+#which will be a2+d3
+#Considering origin at the 2nd joint,Joints 1 to 3 are like a spherical case.
+#Azimuth angle will be 180 and Polar angle will be -x degrees. 
+#theta1 will be the default initial value luckily. theta2 will be -x degrees.
+#d3 will be distance from joint 3 to joint 4 based on a human's back measuments.
+
+#For joint 4 and 5, the distance from the elbow to the wrist(joint 6) will
+#determine the angles for the joints 4 and 5. So that will lead to an obtuse
+#tringle being formed. The angles of this obtuse tringle will be placed over 
+#the 2 joints and we will get the joint values.
+
+#For joint 6, the angle will depend on which direction the gun needs to point.
+#That will be adjusted over the offset angle of joint 6 and we will get the
+#joint angle for the final rotational joint.
